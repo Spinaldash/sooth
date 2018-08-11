61f96a3321f60a518164c92f1e3d23475e2b40c3
MJ Perez <blueavenger@gmail.com>
2018-08-11 10:06:35 -0700

    Scaffolding

---
+++ src/components/Forge.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Forge extends Component {
+  render() {
+    return (
+      <div className="Forge">
+        <div>Hello World</div>
+      </div>
+    );
+  }
+}
+
+export default Forge;
